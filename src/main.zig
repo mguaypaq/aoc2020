b9a77326bacb0e851e50c9dbe7c57b2b60a2587f
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2020-12-04 13:39:56 -0500

    Solve day 4 part 2.
    This time I'm using function pointers! Generically, each passport
    field has a name and a validator, and the validator is a function.
    Sadly (?), it looks like Zig doesn't have lambdas, so in the end
    every function needs a global name. It's like they don't even *want*
    the programmer to confuse readers.
    
    I'm reasonably happy with the resulting code.
    
    $ zig build run
    123
    
    Interestingly, so far I've had no logic errors, and once the code
    runs to completion, the answer is correct on the first try. Even
    when I don't try the example inputs from the puzzle descriptions.
    I should probably write tests anyway.

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,5 @@
 const std = @import("std");
 
-const required_fields = init: {
-    var fields = [_][]const u8{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
-    for (fields) |prefix, index| fields[index] = prefix ++ ":";
-    break :init fields;
-};
-
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -23,9 +17,11 @@ pub fn main() !void {
         var kv_pairs = std.mem.tokenize(passport, " \n");
         var seen = [_]bool{false} ** required_fields.len;
         while (kv_pairs.next()) |kv_pair| {
-            inline for (required_fields) |prefix, index| {
+            inline for (required_fields) |field, index| {
+                const prefix = field.name ++ ":";
                 if (std.mem.startsWith(u8, kv_pair, prefix)) {
-                    seen[index] = true;
+                    const value = kv_pair[(prefix.len)..];
+                    if (field.validate(value)) seen[index] = true;
                 }
             }
         }
@@ -33,4 +29,70 @@ pub fn main() !void {
     }
 
     try std.io.getStdOut().writer().print("{}\n", .{valid_count});
+}
+
+const RequiredField = struct {
+    name: []const u8,
+    validate: fn (value: []const u8) bool,
+};
+
+const required_fields = [_]RequiredField{
+    .{ .name = "byr", .validate = byrValidate },
+    .{ .name = "iyr", .validate = iyrValidate },
+    .{ .name = "eyr", .validate = eyrValidate },
+    .{ .name = "hgt", .validate = hgtValidate },
+    .{ .name = "hcl", .validate = hclValidate },
+    .{ .name = "ecl", .validate = eclValidate },
+    .{ .name = "pid", .validate = pidValidate },
+};
+
+fn byrValidate(buf: []const u8) bool {
+    if (buf.len != 4) return false;
+    const num = std.fmt.parseUnsigned(u32, buf, 10) catch return false;
+    return (1920 <= num and num <= 2002);
+}
+
+fn iyrValidate(buf: []const u8) bool {
+    if (buf.len != 4) return false;
+    const num = std.fmt.parseUnsigned(u32, buf, 10) catch return false;
+    return (2010 <= num and num <= 2020);
+}
+
+fn eyrValidate(buf: []const u8) bool {
+    if (buf.len != 4) return false;
+    const num = std.fmt.parseUnsigned(u32, buf, 10) catch return false;
+    return (2020 <= num and num <= 2030);
+}
+
+fn hgtValidate(buf: []const u8) bool {
+    if (std.mem.endsWith(u8, buf, "cm")) {
+        const num = std.fmt.parseUnsigned(u32, buf[0..(buf.len - 2)], 10) catch return false;
+        return (150 <= num and num <= 193);
+    } else if (std.mem.endsWith(u8, buf, "in")) {
+        const num = std.fmt.parseUnsigned(u32, buf[0..(buf.len - 2)], 10) catch return false;
+        return (59 <= num and num <= 76);
+    } else return false;
+}
+
+fn hclValidate(buf: []const u8) bool {
+    if (buf.len != 7) return false;
+    if (buf[0] != '#') return false;
+    for (buf[1..]) |char| switch (char) {
+        '0'...'9' => continue,
+        'a'...'f' => continue,
+        else => return false,
+    }
+    else return true;
+}
+
+fn eclValidate(buf: []const u8) bool {
+    const colors = [_][]const u8{ "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+    for (colors) |color| if (std.mem.eql(u8, buf, color)) return true;
+    return false;
+}
+
+fn pidValidate(buf: []const u8) bool {
+    if (buf.len != 9) return false;
+    for (buf) |char| if (char < '0' or '9' < char) return false;
+    return true;
 }
