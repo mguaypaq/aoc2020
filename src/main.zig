65da77464fc1e77d148492b23df2a2966661e942
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2020-12-12 01:35:28 -0500

    Solve day 9 part 2.
    This time, I learned from the test case that I should read more
    carefully (I initially returned first-in-range plus last-in-range,
    rather than min-in-range plus max-in-range), thankfully very
    quickly, with the help of std.debug.print().
    
    For this part I'm pleased that I went with a slightly clever
    algorithm, where the contiguous slice of numbers under consideration
    either grows or shrinks at each step, and something very close to
    the minimal amount of work is done to find the target slice.
    
    On to day 10!

--- src/main.zig
+++ src/main.zig
@@ -19,7 +19,7 @@ pub fn main() !void {
         try xmas.append(number);
     }
 
-    try std.io.getStdOut().writer().print("{}\n", .{firstInvalid(xmas.items, 25)});
+    try std.io.getStdOut().writer().print("{}\n", .{encryptionWeakness(xmas.items, 36845998)});
 }
 
 test "example" {
@@ -45,17 +45,31 @@ test "example" {
         309,
         576,
     };
-    std.testing.expectEqual(@as(?u64, 127), firstInvalid(&xmas, 5));
+    std.testing.expectEqual(@as(u64, 62), encryptionWeakness(&xmas, 127));
 }
 
-fn firstInvalid(xmas: []const u64, window_size: usize) ?u64 {
-    for (xmas) |target, target_index| {
-        if (target_index < window_size) continue;
-        const window = xmas[(target_index - window_size)..target_index];
-        search: for (window) |right, right_index| {
-            for (window[0..right_index]) |left| {
-                if (left + right == target) break :search;
-            }
-        } else return target;
-    } else return null;
+fn encryptionWeakness(xmas: []const u64, target: u64) u64 {
+    var left: usize = 0;
+    var right: usize = 1;
+    var sum: u64 = xmas[0] + xmas[1];
+    while (sum != target) if (sum < target) {
+        right += 1;
+        sum += xmas[right];
+    } else {
+        sum -= xmas[left];
+        left += 1;
+        std.debug.assert(left < right);
+    };
+
+    var min = xmas[right];
+    for (xmas[left..right]) |val| {
+        if (min > val) min = val;
+    }
+
+    var max = xmas[right];
+    for (xmas[left..right]) |val| {
+        if (max < val) max = val;
+    }
+
+    return min + max;
 }
