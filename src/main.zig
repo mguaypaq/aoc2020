ac6de5f4a326922a7f0d91f92bda8d1d4d5a709f
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2020-12-14 02:24:25 -0500

    Solve day 13 part 2.
    Hurray for the Chinese Remainder Theorem and Bézout's identity and
    the Extended Euclidean Algorithm! My implementation uses variable
    names with more than one letter each, and spells out the notable
    invariants that it maintains.
    
    I'm glad I made my implementation generic with respect to the width
    of the integers used, because I overflowed 64 bits, and was able to
    easily swap in 128 bits. (I also got a slight thrill at the thought
    that I could have just as easily swapped in 65535 bits.)
    
    $ zig build run
    741745043105674
    
    On to day 14.

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,53 @@
 const std = @import("std");
+const math = std.math;
 
-pub fn main() !void {
-    var best_id: i32 = undefined;
-    var best_wait: i32 = std.math.maxInt(i32);
-    for (buses) |bus| if (bus) |id| {
-        const wait = @mod(-earliest, id);
-        if (wait < best_wait) {
-            best_id = id;
-            best_wait = wait;
+fn ModClass(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        remainder: T,
+        divisor: T,
+
+        fn init(remainder: T, divisor: T) !Self {
+            return Self{
+                .remainder = try math.mod(T, remainder, divisor),
+                .divisor = divisor,
+            };
+        }
+
+        fn intersectWith(self: *Self, other: Self) !void {
+            // These vectors maintain the following two invariants:
+            // vec[0] == vec[1] * self.divisor + vec[2] * other.divisor
+            // abs(current[1] * previous[2] - current[2] * previous[1]) == 1
+            var previous = [3]T{ self.divisor, 1, 0 };
+            var current = [3]T{ other.divisor, 0, 1 };
+            while (current[0] != 0) {
+                const quotient = try math.divFloor(T, previous[0], current[0]);
+                var next = [3]T{
+                    previous[0] - quotient * current[0],
+                    previous[1] - quotient * current[1],
+                    previous[2] - quotient * current[2],
+                };
+                previous = current;
+                current = next;
+            }
+            // At this point, previous[0] is the greatest common divisor,
+            // and previous[1], previous[2] are the Bézout coefficients.
+            self.remainder += previous[1] * self.divisor * try math.divExact(T, other.remainder - self.remainder, previous[0]);
+            self.divisor *= try math.divExact(T, other.divisor, previous[0]);
+            self.remainder = try math.mod(T, self.remainder, self.divisor);
         }
     };
+}
 
-    try std.io.getStdOut().writer().print("{}\n", .{best_id * best_wait});
+pub fn main() !void {
+    var coincidence = try ModClass(i128).init(0, 1);
+    for (buses) |bus, offset| if (bus) |id| {
+        const constraint = try ModClass(i128).init(-@as(i128, offset), id);
+        try coincidence.intersectWith(constraint);
+    };
+
+    try std.io.getStdOut().writer().print("{}\n", .{coincidence.remainder});
 }
 
 const earliest = 1002460;
