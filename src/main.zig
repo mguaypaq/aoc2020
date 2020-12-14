530461118c93bf14d08e8cbd5cfef30e973ddd5f
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2020-12-14 00:02:32 -0500

    Solve day 12 part 2.
    I thought I was going to need 3 points, each with x and y coordinates,
    and somehow that was my threshold for creating a Vec struct for two-
    dimensional vectors. In the end there were only 2 points, but it did
    let me see if I could do parallel assignment of two values without
    using a temporary variable, like:
    
    ```zig
    waypoint = Vec{
        .x = -waypoint.y,
        .y = waypoint.x,
    };
    ```
    
    Sadly no: here Zig first computes the new `x` from the old `y`, then
    computes the new `y` from the new `x`, rather than the old `x`.
    
    But thanks to Zig's ability to print struct values nicely by default,
    and the puzzle text's very verbose walkthrough of the example, this
    was quick to debug.
    
    $ zig build run
    63447
    
    On to day 13.

--- src/main.zig
+++ src/main.zig
@@ -9,6 +9,8 @@ fn abs(x: i32) i32 {
     return if (x < 0) -x else x;
 }
 
+const Vec = struct { x: i32, y: i32 };
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -18,44 +20,45 @@ pub fn main() !void {
     defer text.deinit();
     try file.reader().readAllArrayList(&text, 1024 * 1024);
 
-    // Start at the origin.
-    var x: i32 = 0;
-    var y: i32 = 0;
-
-    // Facing east.
-    var dx: i32 = 1;
-    var dy: i32 = 0;
+    var ship = Vec{ .x = 0, .y = 0 };
+    var waypoint = Vec{ .x = 10, .y = 1 };
 
     var tokens = std.mem.tokenize(text.items, "\n");
     while (tokens.next()) |token| {
         const action = token[0];
         const value = try std.fmt.parseUnsigned(i32, token[1..], 10);
         switch (action) {
-            'N' => y += value,
-            'S' => y -= value,
-            'E' => x += value,
-            'W' => x -= value,
+            'N' => waypoint.y += value,
+            'S' => waypoint.y -= value,
+            'E' => waypoint.x += value,
+            'W' => waypoint.x -= value,
             'L', 'R' => {
-                if (eql(token, "L90") or eql(token, "R270")) {
-                    const new_dx = -dy;
-                    dy = dx;
-                    dx = new_dx;
-                } else if (eql(token, "L180") or eql(token, "R180")) {
-                    dx = -dx;
-                    dy = -dy;
-                } else if (eql(token, "L270") or eql(token, "R90")) {
-                    const new_dx = dy;
-                    dy = -dx;
-                    dx = new_dx;
-                } else return error.UnsupportedAngle;
+                const new_waypoint = if (eql(token, "L90") or eql(token, "R270"))
+                    Vec{
+                        .x = -waypoint.y,
+                        .y = waypoint.x,
+                    }
+                else if (eql(token, "L180") or eql(token, "R180"))
+                    Vec{
+                        .x = -waypoint.x,
+                        .y = -waypoint.y,
+                    }
+                else if (eql(token, "L270") or eql(token, "R90"))
+                    Vec{
+                        .x = waypoint.y,
+                        .y = -waypoint.x,
+                    }
+                else
+                    return error.UnsupportedAngle;
+                waypoint = new_waypoint;
             },
             'F' => {
-                x += dx * value;
-                y += dy * value;
+                ship.x += waypoint.x * value;
+                ship.y += waypoint.y * value;
             },
             else => return error.InvalidCharacter,
         }
     }
 
-    try std.io.getStdOut().writer().print("{}\n", .{abs(x) + abs(y)});
+    try std.io.getStdOut().writer().print("{}\n", .{abs(ship.x) + abs(ship.y)});
 }
