36de7a99af576a21478486b366311655785aeebc
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2020-12-27 00:36:30 -0500

    Solve day 22 part 2.
    A complete redesign of the game: since we're making copies of arrays
    all over the place to keep a history of each game state, there's no
    need to be cute with a circular buffer anymore.
    
    Each sub-game uses its own ArenaAllocator to allocate copies of decks
    of cards, then frees the whole arena once the sub-game is done. As a
    small wrinkle, the final deck of cards from the sub-game returned to
    the parent is allocated globally, and the parent is responsible for
    freeing it.
    
    Since `game()` calls itself recursively, I had to specify the set of
    possible errors it returns explicitly, which seems reasonable.
    
    I guess I was very careful when transcribing the rules into code,
    because once the syntax errors were fixed, the program gave the right
    answer on the first run (after a suspenseful 10 seconds of wondering
    whether I accidentally put in an infinite loop).
    
    $ zig build
    $ time ./zig-cache/bin/aoc-2020
    31657
    
    real	0m10.601s
    user	0m9.722s
    sys	0m0.877s
    
    On to day 23!

--- src/main.zig
+++ src/main.zig
@@ -1,73 +1,98 @@
 const std = @import("std");
+const mem = std.mem;
 const assert = std.debug.assert;
 const ArrayList = std.ArrayList;
 
-var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-const allocator = &arena.allocator;
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const global_allocator = &gpa.allocator;
+
+const Card = u8;
+const Deck = []const Card;
+const Decks = [2]Deck;
+const History = ArrayList(Decks);
+const Player = u1;
+const Result = struct { winner: Player, deck: Deck };
+
+fn game(initial_decks: Decks) mem.Allocator.Error!Result {
+    var arena = std.heap.ArenaAllocator.init(global_allocator);
+    defer arena.deinit();
+    const local_allocator = &arena.allocator;
+
+    var history = History.init(local_allocator);
+    var decks = initial_decks;
+    while (true) {
+        // Check for empty decks
+        for (decks) |deck, player| {
+            if (deck.len == 0) {
+                const winner: Player = @intCast(u1, 1 - player);
+                const winning_deck = try global_allocator.alloc(Card, decks[winner].len);
+                mem.copy(Card, winning_deck, decks[winner]);
+                return Result{
+                    .winner = winner,
+                    .deck = winning_deck,
+                };
+            }
+        }
+
+        // Check for repeats
+        for (history.items) |old_decks| {
+            if (!mem.eql(Card, old_decks[0], decks[0])) continue;
+            if (!mem.eql(Card, old_decks[1], decks[1])) continue;
+            const deck = try global_allocator.alloc(Card, decks[0].len);
+            mem.copy(Card, deck, decks[0]);
+            return Result{
+                .winner = 0,
+                .deck = deck,
+            };
+        }
+        try history.append(decks);
+
+        const draws = [2]Card{ decks[0][0], decks[1][0] };
+        const round_winner: Player = if (draws[0] < decks[0].len and
+            draws[1] < decks[1].len)
+        round_winner: {
+            // Recursive game
+            const result = try game(Decks{
+                decks[0][1..(1 + draws[0])],
+                decks[1][1..(1 + draws[1])],
+            });
+            global_allocator.free(result.deck);
+            break :round_winner result.winner;
+        } else if (draws[0] > draws[1])
+            @as(Player, 0)
+        else if (draws[0] < draws[1])
+            @as(Player, 1)
+        else
+            unreachable;
+        const round_loser: Player = 1 - round_winner;
+
+        var new_decks: [2][]Card = undefined;
+
+        new_decks[round_winner] = try local_allocator.alloc(Card, decks[round_winner].len + 1);
+        mem.copy(Card, new_decks[round_winner], decks[round_winner][1..]);
+        new_decks[round_winner][decks[round_winner].len - 1] = draws[round_winner];
+        new_decks[round_winner][decks[round_winner].len] = draws[round_loser];
+
+        new_decks[round_loser] = try local_allocator.alloc(Card, decks[round_loser].len - 1);
+        mem.copy(Card, new_decks[round_loser], decks[round_loser][1..]);
+
+        decks = new_decks;
+    }
+}
 
 pub fn main() !void {
-    var hands = [2]Hand{ Hand{}, Hand{} };
-    for (hands) |*hand, player|
-        for (input[player]) |card|
-            hand.append(card);
+    const result = try game(input);
+    defer global_allocator.free(result.deck);
 
-    var draws: [2]Card = undefined;
-    while (!hands[0].empty() and !hands[1].empty()) {
-        for (hands) |*hand, player|
-            draws[player] = hand.draw();
-        if (draws[0] < draws[1]) {
-            hands[1].append(draws[1]);
-            hands[1].append(draws[0]);
-        } else if (draws[1] < draws[0]) {
-            hands[0].append(draws[0]);
-            hands[0].append(draws[1]);
-        } else unreachable;
-    }
-
-    const winner = for (hands) |hand, player| {
-        if (!hand.empty()) break player;
-    } else unreachable;
-
-    var score: u32 = 0;
-    var value: u32 = deck_size;
-    while (value > 0) : (value -= 1) {
-        score += value * hands[winner].draw();
+    var score: usize = 0;
+    for (result.deck) |card, index| {
+        score += card * (result.deck.len - index);
     }
 
     try std.io.getStdOut().writer().print("{}\n", .{score});
 }
 
-const input = [_][]const Card{
+const input = Decks{
     &[_]Card{ 47, 19, 22, 31, 24, 6, 10, 5, 1, 48, 46, 27, 8, 45, 16, 28, 33, 41, 42, 36, 50, 39, 30, 11, 17 },
     &[_]Card{ 4, 18, 21, 37, 34, 15, 35, 38, 20, 23, 9, 25, 32, 13, 26, 2, 12, 44, 14, 49, 3, 40, 7, 43, 29 },
-};
-
-const deck_size = sum: {
-    var size: usize = 0;
-    for (input) |hand| size += hand.len;
-    break :sum size;
-};
-
-const Card = u8;
-const Hand = struct {
-    buf: [deck_size + 1]Card = undefined,
-    head: usize = 0,
-    tail: usize = 0,
-
-    fn empty(self: Hand) bool {
-        return (self.head == self.tail);
-    }
-
-    fn append(self: *Hand, card: Card) void {
-        self.buf[self.tail] = card;
-        self.tail = (self.tail + 1) % self.buf.len;
-        assert(!self.empty());
-    }
-
-    fn draw(self: *Hand) Card {
-        assert(!self.empty());
-        const card = self.buf[self.head];
-        self.head = (self.head + 1) % self.buf.len;
-        return card;
-    }
 };
