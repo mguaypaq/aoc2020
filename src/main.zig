25ed673d9c82ba0a5bd44b0317c5d993b9ad69b4
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2020-12-28 00:06:38 -0500

    Test run for day 24 part 2.
    And a good thing too, this test helped me fix a logic bug!

--- src/main.zig
+++ src/main.zig
@@ -1,23 +1,94 @@
 const std = @import("std");
 const assert = std.debug.assert;
+const sort = std.sort;
 const ArrayList = std.ArrayList;
 
 var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 const allocator = &arena.allocator;
 
-const input = @embedFile("../input.txt");
+const input = @embedFile("../test-input.txt");
 
 pub fn main() !void {
     try parseInput();
-    try std.io.getStdOut().writer().print("{}\n", .{flipped_tiles.items.len});
+    const writer = std.io.getStdOut().writer();
+    try writer.print("Initial: {}\n", .{flipped_tiles.items.len});
+    var day: usize = 0;
+    while (day < 100) {
+        day += 1;
+        try step();
+        try writer.print("Day {}: {}\n", .{ day, flipped_tiles.items.len });
+    }
 }
 
-const Tile = struct { x: isize, y: isize };
+fn step() !void {
+    if (flipped_tiles.items.len == 0) return;
+
+    // Mapper
+    try mapped_tiles.resize(0);
+    try mapped_tiles.ensureCapacity(6 * flipped_tiles.items.len);
+    for (flipped_tiles.items) |t| {
+        try mapped_tiles.append(Tile{ .x = t.x + 1, .y = t.y + 0 });
+        try mapped_tiles.append(Tile{ .x = t.x + 1, .y = t.y - 1 });
+        try mapped_tiles.append(Tile{ .x = t.x + 0, .y = t.y - 1 });
+        try mapped_tiles.append(Tile{ .x = t.x - 1, .y = t.y + 0 });
+        try mapped_tiles.append(Tile{ .x = t.x - 1, .y = t.y + 1 });
+        try mapped_tiles.append(Tile{ .x = t.x + 0, .y = t.y + 1 });
+    }
+    sort.sort(Tile, mapped_tiles.items, {}, Tile.lessThan);
+
+    // Reducer
+    try new_tiles.resize(0);
+    var f: usize = 0;
+    var m: usize = 0;
+    while (m < mapped_tiles.items.len) {
+        const tile = mapped_tiles.items[m];
+
+        while ((f < flipped_tiles.items.len) and
+            Tile.lessThan({}, flipped_tiles.items[f], tile))
+        {
+            f += 1;
+        }
+        const black = (f < flipped_tiles.items.len) and
+            tile.equal(flipped_tiles.items[f]);
+
+        var count: usize = 0;
+        while ((m < mapped_tiles.items.len) and
+            tile.equal(mapped_tiles.items[m]))
+        {
+            count += 1;
+            m += 1;
+        }
+
+        if ((black and count == 1) or count == 2) {
+            try new_tiles.append(tile);
+        }
+    }
+
+    // Swap in the result
+    const tmp = flipped_tiles;
+    flipped_tiles = new_tiles;
+    new_tiles = tmp;
+}
+
+const Tile = struct {
+    x: isize,
+    y: isize,
+
+    fn equal(self: Tile, other: Tile) bool {
+        return (self.x == other.x) and (self.y == other.y);
+    }
+
+    fn lessThan(context: void, lhs: Tile, rhs: Tile) bool {
+        return (lhs.x < rhs.x) or ((lhs.x == rhs.x) and (lhs.y < rhs.y));
+    }
+};
 var flipped_tiles = ArrayList(Tile).init(allocator);
+var mapped_tiles = ArrayList(Tile).init(allocator);
+var new_tiles = ArrayList(Tile).init(allocator);
 
 fn flip(tile: Tile) !void {
     for (flipped_tiles.items) |t, i| {
-        if (tile.x == t.x and tile.y == t.y) {
+        if (tile.equal(t)) {
             _ = flipped_tiles.swapRemove(i);
             return;
         }
@@ -32,6 +103,7 @@ fn parseInput() !void {
         const tile = try input_parser.parseLine();
         try flip(tile);
     }
+    sort.sort(Tile, flipped_tiles.items, {}, Tile.lessThan);
 }
 
 const InputParser = struct {
