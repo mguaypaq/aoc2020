90cb1c18523f414a50ce412a8e04574c26ef8c70
Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>
2020-12-28 01:12:01 -0500

    Solve day 25 part 1.
    Ok, fine, these are small numbers, let's just brute-force it.
    
    $ zig build run
    10548634
    
    I considered doing that trick which reduces the worst case from
    20201226 to 2*sqrt(20201226), but that would take more work.
    
    On to part 2.

--- src/main.zig
+++ src/main.zig
@@ -5,22 +5,24 @@ const ArrayList = std.ArrayList;
 var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 const allocator = &arena.allocator;
 
-const base = 7;
+const generator = 7;
 const modulus = 20201227;
 const public_keys = [_]u64{ 6930903, 19716708 };
-const order = modulus - 1;
-const factors = [_]u64{ 2, 3, 29, 116099 };
 
 pub fn main() !void {
     const writer = std.io.getStdOut().writer();
 
-    for (factors) |factor| {
-        try writer.print("{}\n", .{pow(order / factor)});
+    var private: u64 = 0;
+    var public: u64 = 1;
+    while (public != public_keys[0]) {
+        private += 1;
+        public = public * generator % modulus;
     }
+    try writer.print("{}\n", .{pow(public_keys[1], private)});
 }
 
 // Compute `base` ** `exponent` % `modulus`.
-fn pow(exponent: u64) u64 {
+fn pow(base: u64, exponent: u64) u64 {
     var result: u64 = 1;
     var cur_base: u64 = base;
     var cur_exp = exponent;
